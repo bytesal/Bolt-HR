53d78a72fa4dff1896d8053f69185c7a13a5282c
Sal <35301371+bytesal@users.noreply.github.com>
2026-05-22 14:19:05 +0300

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,3 +1,11 @@
-from .database import db
+import os
+import motor.motor_asyncio
 
-__all__ = ['db']
+class Database:
+    def __init__(self):
+        uri = os.getenv('MONGODB_URI')
+        print(f"🔍 MONGODB_URI = {uri}")  # ← سطر تشخيصي
+        db_name = os.getenv('MONGODB_DB_NAME', 'discord_bot')
+        self.client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+        self.db = self.client[db_name]
+    # ... باقي الكود كما هو
